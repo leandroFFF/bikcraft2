15116cebb68e0908a930bc1949ff8170cf4c9b6b
leandroffdev <leandrofrancagti@gmail.com>
2024-06-10 00:19:25 -0300

    adicionadas funcionalidades de animacao com Javascript

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function ativarLink(link) {
         link.classList.add("ativo");
     }
 }
-links.forEach(ativarLink)
+links.forEach(ativarLink);
 
 // Ativar itens do orçamento
 const parametros = new URLSearchParams(location.search);
@@ -17,7 +17,46 @@ function ativarProduto(parametro) {
     const elemento = document.getElementById(parametro);
     if(elemento) {
         elemento.checked = true;
-        console.log(elemento);
+        // console.log(elemento);
     }
 }
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicleta
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px').matches;
+    console.log(media);
+    if(media){
+        galeriaContainer.prepend(img);
+    }
+}
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+galeria.forEach(eventosGaleria);
+
+// Anime
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}
+if(window.ClipboardJS) {
+    new ClipboardJS('.introducao-conteudo p');
+}
